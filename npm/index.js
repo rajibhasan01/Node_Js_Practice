c1d3f8891b41613497d52d11cb657bc129c4912f
rajibhasan01 <hasan.rajib1996@gmail.com>
2022-04-05 00:37:36 +0600

    updated

--- npm/index.js
+++ npm/index.js
@@ -1,2 +1,5 @@
-const colors = require('colors');
-console.log('Md. Rajib Hasan'.bgRed)
+// const colors = require('colors');
+// console.log('Md. Rajib Hasan'.bgRed)
+
+import chalk from 'chalk';
+console.log(chalk.blue('Hello world!'));
