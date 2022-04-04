cdbd202485f6cb6f53544c17d99d0faa9b77bde9
rajibhasan01 <hasan.rajib1996@gmail.com>
2022-04-04 23:52:14 +0600

    updated

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 module.exports ={
      x: 10,
-     y:20
+     y:20,
+
+     z:function(){
+         return 10;
+     }
 }
--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const app = require('./app')
 
 console.log(app.x);
+
+console.log(app.z());
+
+
+const arr = [2,4,7,1,3,8,3];
+
+const new_arr = arr.filter((item)=>{return item >3})
+console.log(new_arr);
