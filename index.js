edb38c366da82f37b1150cd33ea8a19467d02523
rajibhasan01 <hasan.rajib1996@gmail.com>
2022-04-05 00:00:54 +0600

    updated

--- index.js
+++ index.js
@@ -9,3 +9,11 @@ const arr = [2,4,7,1,3,8,3];
 
 const new_arr = arr.filter((item)=>{return item >3})
 console.log(new_arr);
+
+
+
+// core module
+const fs = require('fs');
+fs.writeFileSync("hello.txt", "new file created");
+
+console.log("->>", __dirname);
