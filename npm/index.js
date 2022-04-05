1f93527c2dbb414f3d0e99cd6275fbc13807838a
rajibhasan01 <hasan.rajib1996@gmail.com>
2022-04-05 23:20:16 +0600

    updated

--- npm/index.js
+++ npm/index.js
@@ -1,5 +1,14 @@
 // const colors = require('colors');
-// console.log('Md. Rajib Hasan'.bgRed)
+import colors from 'colors';
+console.log('Md. Rajib Hasan'.bgRed)
 
 import chalk from 'chalk';
 console.log(chalk.blue('Hello world!'));
+
+import http from 'http';
+
+http.createServer((req, res)=>{
+    res.writeHead(200,{'Content-Type':'application\json'});
+    res.write(JSON.stringify({name:'Md. Rajib Hasan', email:'hasan.rajib1996@gmail.com'}));
+    res.end();
+}).listen(5000);
