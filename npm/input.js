d9c0e9dabfd42a27b1ae0586eeda4e3361517e6a
rajibhasan01 <hasan.rajib1996@gmail.com>
2022-04-05 23:44:27 +0600

    updated

--- npm/input.js
+++ npm/input.js
@@ -1,5 +1,15 @@
 import fs from 'fs';
 
 const input = process.argv;
-fs.writeFileSync(input[2],input[3]);
+if (input[2] == 'add'){
+    fs.writeFileSync(input[3],input[4]);
+}
+
+else if(input[2] == 'remove'){
+    fs.unlinkSync(input[3]);
+}
+else{
+    console.log('invalid input');
+}
+// fs.writeFileSync(input[2],input[3]);
 console.log(process.argv[2]);
