25c5ac6d9faa94974883e2a8e7bc481d17a8f909
rajibhasan01 <hasan.rajib1996@gmail.com>
2022-04-17 16:34:35 +0600

    updated

--- mongoose/index.js
+++ mongoose/index.js
@@ -1,37 +1,21 @@
 import express from "express";
-import './config.js';
+import "./config.js";
 import { product } from "./product.js";
+
 const app = express();
 app.use(express.urlencoded({ extended : true }));
 app.use(express.json());
 
 
-app.post('/create', async (req, res) =>{
-    let data = new product(req.body);
-    let result = await data.save();
-    console.log(result);
-    res.send(result);
-});
-
-app.get("/list", async(req, res) =>{
-    let data = await product.find();
-    res.send(data);
-});
-
-app.delete("/delete/:_id", async(req, res) =>{
-    const id = req.params;
-    let data = await product.deleteOne(id);
-    res.send(data);
-});
-
-app.put("/update/:_id", async(req, res) =>{
-    const id = req.params;
-    let data = await product.updateOne(
-        id,
-        {
-            $set: req.body
-        }
-    );
+app.get('/search/:key', async(req, res) =>{
+    console.log(req.params.key);
+    let data = await product.find({
+        "$or":[
+            {"name":{$regex:req.params.key}},
+            {"brand":{$regex:req.params.key}},
+            {"category":{$regex:req.params.key}}
+        ]
+    });
     res.send(data);
 });
 
