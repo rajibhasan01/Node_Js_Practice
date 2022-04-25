a875c31df43e22c67e95ce77d374eba75d28d91c
rajibhasan01 <hasan.rajib1996@gmail.com>
2022-04-25 23:57:12 +0600

    updated

--- Learn_with_sumit/RAW-NODE-API-PROJECT/index.js
+++ Learn_with_sumit/RAW-NODE-API-PROJECT/index.js
@@ -8,6 +8,8 @@
 
 // Dependencies
 const http = require('http');
+const url = require('url');
+const {StringDecoder} = require('string_decoder')
 
 // App object - module scaffolding
 const app = {};
@@ -27,8 +29,34 @@ app.creatServer = () =>{
 
 // Handle request response
 app.handleReqRes = (req, res) =>{
+    // Request handling
+    // get the url and parse it
+    const parsedUrl = url.parse(req.url, true);
+    const path = parsedUrl.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+    const method = req.method.toLowerCase();
+    const queryStringObject = parsedUrl.query;
+    const headersObject = req.headers;
+    const decoder = new StringDecoder('utf-8');
+    let realData = '';
+
+    req.on('data', (buffer) => {
+        realData += decoder.write(buffer);
+    });
+
+    req.on('end', ()=>{
+        realData += decoder.end();
+        console.log(realData);
+        res.end('Hello programers');
+    });
+
+    console.log(realData);
+    console.log(headersObject);
+    console.log(queryStringObject);
+    console.log(method);
+    console.log(trimmedPath);
     // response handle
-    res.end('Hello World');
+    // res.end('Hello programers');
 };
 
 // Start the server
